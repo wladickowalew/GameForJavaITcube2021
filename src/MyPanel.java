3440b103c87612deab1b793535c6e029fb6a5b1f
Kovalev <test@gmail.ru>
2021-01-28 17:52:35 +0300

    add grass

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,7 +1,28 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 
 public class MyPanel extends JPanel {
-    public MyPanel(){
+    BufferedImage grass;
 
+    public MyPanel(){
+        setLayout(null);
+        try {
+            grass = ImageIO.read(new File("Images\\grass.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int yg = 0; yg < 600; yg+=50)
+            for (int xg = 0; xg < 800; xg+=50)
+                g.drawImage(grass, xg, yg, null);
     }
 }
